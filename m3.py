8e2f0b54dbd3d2de1a27ff506f6119fcd98e6290
Lucas Andrés Márquez <97174373+marquezlucas@users.noreply.github.com>
2023-10-25 17:51:48 -0300

    punto dos

--- m3.py
+++ m3.py
@@ -1,5 +1,5 @@
 #Modulo 3 
-#Objetos - Básico 
+#1)Objetos - Básico 
 
 #ejercicio 1 - Mascota
 
@@ -71,4 +71,99 @@ class Calculadora:
             ZeroDivisionError
             print('No se puede dividir por cero, pruebe con otro número')
         
-       
+calculo1 = Calculadora(10,2.3)   
+calculo2 = Calculadora(4,0)
+calculo1.suma() 
+calculo1.resta()   
+calculo1.multiplicacion()
+calculo1.division()
+calculo2.division()
+
+
+#ejercicio 4 - Agenda
+
+class Agenda:
+    def __init__(self):
+        self.contactos = []
+
+    def mostrar_menu(self):
+        print("\nMenú:")
+        print("a. Añadir contacto")
+        print("b. Lista de contactos")
+        print("c. Buscar contacto")
+        print("d. Editar contacto")
+        print("e. Cerrar agenda")
+
+    def agregar_contacto(self,nombre,telefono,email):
+        nuevo_contacto = {"nombre":nombre,"telefono":telefono,"email":email}
+        self.contactos.append(nuevo_contacto)
+    
+    def mostrar_contactos(self):
+        for i in self.contactos:
+            print(f'Nombre: {i["nombre"]}, Telefóno: {i["telefono"]}, Email: {i["email"]}')
+
+    def buscar_contacto(self,nombre):
+        for contacto in self.contactos:
+            if contacto['nombre']== nombre:
+                print(f'El nombre es: {contacto["nombre"]}, con telefóno: {contacto["telefono"]}, email: {contacto["email"]}')
+                return
+        print(f"No se encontró un contacto con el nombre {nombre}")
+
+    def editar_contacto(self,nombre):
+        for contacto in self.contactos:
+            if contacto ['nombre']==nombre:
+                print("¿Qué queres editar?")
+                print("1.Nombre")
+                print("2.Telefóno")
+                print("3.Email")
+                opcion = input("Ingrese el número de una de las opciones: ")
+
+                if opcion == '1':
+                    nuevo_nombre = input("Ingrese el nuevo nombre: ")
+                    contacto['nombre'] = nuevo_nombre
+                    print("Nombre actualizado.")
+                elif opcion == '2':
+                    nuevo_telefono = input("Ingrese el nuevo teléfono: ")
+                    contacto['telefono'] = nuevo_telefono
+                    print("Teléfono actualizado.")
+                elif opcion == '3':
+                    nuevo_email = input("Ingrese el nuevo email: ")
+                    contacto['email'] = nuevo_email
+                    print("Email actualizado.")
+                else:
+                    print("Opción no válida.")
+                return
+        print(f"No se encontró un contacto con el nombre {nombre}")
+agenda = Agenda()
+
+while True:
+    agenda.mostrar_menu()
+    opcion = input("Ingrese la opción deseada (a/b/c/d/e): ")
+
+    if opcion == 'a':
+        nombre = input("Ingrese el nombre del contacto: ")
+        telefono = input("Ingrese el teléfono del contacto: ")
+        email = input("Ingrese el email del contacto: ")
+        agenda.agregar_contacto(nombre, telefono, email)
+    elif opcion == 'b':
+        agenda.mostrar_contactos()
+    elif opcion == 'c':
+        nombre = input("Ingrese el nombre del contacto a buscar: ")
+        agenda.buscar_contacto(nombre)
+    elif opcion == 'd':
+        nombre = input("Ingrese el nombre del contacto a editar: ")
+        agenda.editar_contacto(nombre)
+    elif opcion == 'e':
+        break
+    else:
+        print("Opción no válida. Por favor, ingrese a, b, c, d o e.")
+
+#2)Objetos - Clases anidadas 
+
+class Alumno:
+    def __init__(self,nombre,apellido,edad,DNI,materias):
+        self.nombre = str(nombre)
+        self.apellido = str(apellido)
+        self.edad = int(edad)
+        self.DNI = int(DNI)
+        self.materias = materias
