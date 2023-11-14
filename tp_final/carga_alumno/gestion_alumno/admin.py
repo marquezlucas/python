56e5d9542639077f4f2f44e59dbd4de790a0a9d1
marquezlucas <you@example.com>
2023-11-14 17:46:29 -0300

    cambios en views, apps, models, settings

--- tp_final/carga_alumno/gestion_alumno/admin.py
+++ tp_final/carga_alumno/gestion_alumno/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Alumno, Curso, BandaHoraria
+
 
 # Register your models here.
+admin.site.register(Alumno)
+admin.site.register(Curso)
+admin.site.register(BandaHoraria)
--- tp_final/carga_alumno/gestion_alumno/views.py
+++ tp_final/carga_alumno/gestion_alumno/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.shortcuts import get_list_or_404, get_object_or_404
+from .models import Alumno, Curso
+import csv
 
 # Create your views here.
+def cargar_alumno(request,dni): #ver de modificar
+    alumno = get_object_or_404(Alumno, dni = dni)
+
+def listar_alumno(request, dni):
+    alumnos = Alumno.objects.all()
+
+def obtener_alumno(request, dni):
+    alumno = get_object_or_404(Alumno, dni = dni)
+
+def modificar_alumno(request, dni):
+    alumno = get_object_or_404(Alumno, dni = dni)
+
+def eliminar_alumno(request, dni):
+    alumno = get_object_or_404(Alumno, dni = dni)
+
+def asignar_curso(request, dni, id_curso):
+    alumno = get_object_or_404(Alumno, dni = dni)
+
+def alumno_por_curso(request, id_curso):
+    alumno = get_object_or_404(Alumno)
